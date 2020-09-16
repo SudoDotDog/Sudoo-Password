19231d549fed7fa10ca4a8f2187889de2f22db29
WMXPY <wm@mengw.io>
2020-09-16 11:41:36 -0500

    remove unused lint ignore

--- test/unit/saltilizer.test.ts
+++ test/unit/saltilizer.test.ts
@@ -11,13 +11,11 @@ import { Saltilizer } from '../../src';
 
 describe('Given {Saltilizer} class', (): void => {
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const chance: Chance.Chance = new Chance('token-saltilizer');
 
     it('should be able to construct', (): void => {
 
         const salt: string = chance.string();
-
         const saltilizer: Saltilizer = Saltilizer.create(salt);
 
         expect(saltilizer).to.be.instanceOf(Saltilizer);
